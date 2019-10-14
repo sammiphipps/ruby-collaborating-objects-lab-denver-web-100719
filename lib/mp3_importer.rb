4859dcce42c532900122cfe652ef06185ff1554e
Samantha Phipps <sammi.phipps@gmail.com>
2019-10-14 21:39:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,8 @@
 class MP3Importer
-  attr_reader :file_path
+  attr_reader :path
   @@all = []
   def initialize(file_path)
-    @file_path = file_path
+    @path = file_path
     @@all << self 
   end 
 end 
